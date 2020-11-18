d73a26ac575d136ae4671fe09c0a19b3e3ee295c
jasleon <antonio.buap@gmail.com>
2020-11-18 06:58:33 -0600

    Complete linear regression assignment

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,13 +16,16 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    n = length(theta);
+    theta_hat = zeros(n, 1);
+    for j = 1:n
+        G = 0;
+        for i = 1:m
+            G = G + ((theta' * X(i, :)') - y(i)) * X(i, j);
+        end
+        theta_hat(j) = theta(j) - alpha * (m ^ -1) * G;
+    end
+    theta = theta_hat;
     % ============================================================
 
     % Save the cost J in every iteration    
